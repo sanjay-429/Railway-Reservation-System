6ed62a20c3c5b49ccd8248e129e5a2d1cc044574
gupta8808 <sanjayvns8808@gmail.com>
2021-03-05 18:04:13 +0530

    first

---
+++ footer.php
@@ -0,0 +1,99 @@
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4">
+  <div style="background-color: #343a40; color : white; font-size: 12px;">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Railways.Live</h6>
+        <p>This is an initial level website aimed to be useful for booking tickets from anywhere in the world.
+            </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Group Members</h6>
+        <p>
+          <a href="#!">Sanjay Gupta </a>
+        </p>
+        <p>
+          <a href="#!">Akhilesh Singh </a>
+        </p>
+        <p>
+          <a href="#!">Aryan Kumar</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+        <p>
+          <a href="register.php">Register Yourself</a>
+        </p>
+        <p>
+          <a href="pnr.php">PNR Inquiry</a>
+        </p>
+        <p>
+          <a href="train_stas.php">Train Schedule</a>
+        </p>
+        <p>
+          <a href="#!">Help</a>
+        </p>
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+        <p>
+           Uiet Csjmu Kanpur</p>
+        
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2020 Copyright:
+         
+          
+          </a>
+        </p>
+
+      </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+</div>
+
+</footer>
+<!-- Footer -->
+
